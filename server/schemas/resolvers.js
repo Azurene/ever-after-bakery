b41246e2d77f4fa49bf149a5b8bd2285b66b1770
Jodee Ann Conui <98633770+Azurene@users.noreply.github.com>
2022-08-02 20:00:17 -0700

    starting server code and login mutation
    rebase

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,41 @@
+const { AuthenticationError } = require('apollo-server-express');
+// const User = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('thoughts')
+                    .populate('friends');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+    }
+}
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,9 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type Mutation {
+        login(email: String!, password: String!): Auth
+    }
+`
 
 module.exports = typeDefs;
