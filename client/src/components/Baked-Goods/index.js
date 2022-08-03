788b6da5512b6a18c52c874fca844c7def563e05
Rush <kriscookies20@gmail.com>
2022-08-02 23:37:00 -0700

    added in images and partial styling

--- client/src/components/Baked-Goods/index.js
+++ client/src/components/Baked-Goods/index.js
@@ -2,7 +2,277 @@ import React from 'react';
 
 function BakedGoods(){
     return (
-        <h2>Baked Goods</h2>
+        <div>
+            <h1>Baked Goods</h1>
+
+            <section>
+                <h2>carousel</h2>
+            </section>
+
+            <div>
+                <h2>Cakes:</h2>
+                {/* cakes */}
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cakes/IMG_7837.jpg`)}
+                    alt="cake1"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cakes/IMG_7839.jpg`)}
+                    alt="cake2"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cakes/IMG_7841.jpg`)}
+                    alt="cake3"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cakes/IMG_7844.jpg`)}
+                    alt="cake4"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cakes/IMG_7845.jpg`)}
+                    alt="cake5"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cakes/IMG_7846.jpg`)}
+                    alt="cake6"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cakes/IMG_7849.jpg`)}
+                    alt="cake7"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cakes/IMG_7851.jpg`)}
+                    alt="cake8"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cakes/IMG_7854.jpg`)}
+                    alt="cake9"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cakes/IMG_7855.jpg`)}
+                    alt="cake10"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+            
+            </div>
+
+            <div>
+                <h2>Cookies:</h2>
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cookies/IMG_7833.jpg`)}
+                    alt="cookie1"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cookies/IMG_7834.jpg`)}
+                    alt="cookie2"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cookies/IMG_7836.jpg`)}
+                    alt="cookie3"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cookies/IMG_7838.jpg`)}
+                    alt="cookie4"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cookies/IMG_7840.jpg`)}
+                    alt="cookie5"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cookies/IMG_7850.jpg`)}
+                    alt="cookie6"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                 <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cookies/IMG_7852.jpg`)}
+                    alt="cookie7"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cookies/IMG_7853.jpg`)}
+                    alt="cookie8"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cookies/IMG_7856.jpg`)}
+                    alt="cookie9"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/cookies/IMG_7857.jpg`)}
+                    alt="cookie10"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+            </div>
+
+            <div>
+                <h2>Other:</h2>
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/specialty/IMG_7835.jpg`)}
+                    alt="other1"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/specialty/IMG_7842.jpg`)}
+                    alt="other2"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/specialty/IMG_7843.jpg`)}
+                    alt="other3"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/specialty/IMG_7847.jpg`)}
+                    alt="other4"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/specialty/IMG_7848.jpg`)}
+                    alt="other5"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/specialty/IMG_7860.jpg`)}
+                    alt="other6"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/specialty/IMG_7865.jpg`)}
+                    alt="other7"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/specialty/IMG_7868.jpg`)}
+                    alt="other8"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/specialty/IMG_7869.jpg`)}
+                    alt="other9"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+                <img
+                    //src assignd required expression | incremental naming of the imgs using i
+                    // default property --> is where the img has been saved. To render img, default property must be invoked  
+                    src={require(`../../assets/specialty/IMG_7870.jpg`)}
+                    alt="other10"
+                    className="img-thumbnail"
+                    style={{width:"20%"}}
+                />
+            </div>
+
+            <div>
+                <h2>Specialty Items</h2>
+            </div>
+
+            <p>Everything I make is custom. I take what ideas my clients have and turn them into delicious edible art pieces.</p>
+            
+        </div>
+
+        
+        
     )
 }
 
