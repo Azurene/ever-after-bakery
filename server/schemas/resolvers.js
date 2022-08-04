9d093cfa2906609c3d1836daebe0d97721c2e1a8
Jodee Ann Conui <98633770+Azurene@users.noreply.github.com>
2022-08-03 19:27:23 -0700

    changes to server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,9 +7,7 @@ const resolvers = {
         me: async (parent, args, context) => {
             if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id })
-                    .select('-__v -password')
-                    .populate('thoughts')
-                    .populate('friends');
+                    .select('-__v -password');
 
                 return userData;
             }
