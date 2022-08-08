e7a7bb345c3092da269d3ebe8e5bb483c6efeaea
Angel Santos <iiisantosjangel@gmail.com>
2022-08-08 09:09:13 -0700

    updated components, Header/index.js. add buttons to nav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,7 +4,7 @@ function Header(props) {
     return (
 
         <header>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-black">
                 {/* <a className="navbar-brand" href="/"></a> */}
                 {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -13,35 +13,36 @@ function Header(props) {
                     <ul className="navbar-nav mr-auto">
                         {/* home */}
                         <li className="nav-item active">
-                            <a className="nav-link" onClick={() => {
-                            props.setCurrentPage("Header")}} >Home <span className="sr-only"> </span></a>
+                            <a className="nav-link  "><button type='button' class='btn btn-black text-white'  onClick={() => {
+                            props.setCurrentPage("Header")}} >Home</button> <span  className="sr-only"> </span></a>
                         </li>
                         {/* about */}
-                        <li className="nav-item">
+                        <li className="nav-item active">
+                            <a className='nav-link'><button type='button' class='btn btn-black text-white' onClick={() => {
+                                props.setCurrentPage('About')}}>About</button><span className='sr-only'></span> </a>
                             
-                            <button onClick={() => {
-                                props.setCurrentPage("About")}}>
-                                    About Me</button> 
-                                    
-                        </li>
+                                    </li>
                         {/* baked goods */}
-                        <li className="nav-item">
-                            <a className="nav-link" href="/Baked-Goods">Baked Goods</a>
+                        <li className="nav-item active">
+                            <a className="nav-link"><button type='button' class='btn btn-black text-white' onClick={() => {
+                                props.setCurrentPage('Baked-Goods')}}>Baked Goods</button><span  className='sr-only'></span></a>
                         </li>
                         {/* newsletter */}
-                        <li className="nav-item">
-                            <a className="nav-link" href="/Newsletter">Newsletter</a>
+                        <li className="nav-item active">
+                            <a className="nav-link"><button type='button' class='btn btn-black text-white' onClick={() => { 
+                            props.setCurrentPage('Newsletter')}}>Newsletter</button><span  className='sr-only'></span></a>
                         </li>
                         {/* contact */}
-                        <li className="nav-item">
-                            <a className="nav-link" href="/Contact">Contact</a>
+                        <li className="nav-item active">
+                            <a className="nav-link"><button type='button' class='btn btn-black text-white' onClick={()  => {
+                                props.setCurrentPage('Contact')}}>Contact</button><span className='sr-only'></span></a>
                         </li>
                     </ul>
                     <span className="navbar-text text-white ">
                         <a className="navbar" href="/">
                             <div className="logo-image">
                                 <img 
-                                src={require(`../../assets/images/logo/logo.JPEG`)} 
+                                src={require(`../../Assets/images/logo/logo.jpeg`)} 
                                 alt='logo' 
                                 className="img-fluid">
 
