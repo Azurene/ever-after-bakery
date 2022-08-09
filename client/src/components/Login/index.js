3d37659278fa6ad2b40a60ffa1115520c617d4e0
Jodee Ann Conui <98633770+Azurene@users.noreply.github.com>
2022-08-08 23:59:56 -0700

    starting for login functionality

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,23 +1,63 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-function Login() {
+
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
+
+import Auth from '../../utils/auth';
+
+function Login(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
+  
   return (
     <Form className='contactform container'>
       <Form.Group className="mb-3" controlId="formGroupEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
-    <Form.Group className="mb-3" controlId="Textarea">
+      <Form.Group className="mb-3" controlId="Textarea">
         <Form.Label>Please Contact for any general questions:</Form.Label>
         <Form.Control as="textarea" rows={3} />
       </Form.Group>
       <Button type="submit">Login</Button>
-      </Form>
-     
-       
-     
-    
+    </Form>
+
+
+
+
   );
 }
 
