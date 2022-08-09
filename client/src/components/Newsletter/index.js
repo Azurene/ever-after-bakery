60aa17b68c871ea7cd58ab9680e01620664bc503
Rush <kriscookies20@gmail.com>
2022-08-08 21:50:29 -0700

    newsletter spacing & css

--- client/src/components/Newsletter/index.js
+++ client/src/components/Newsletter/index.js
@@ -3,57 +3,68 @@ import React from 'react';
 function Newsletter() {
     return (
         <section>
-            <h1>Upcoming Events</h1>
-            <div>
+            <h1 className='text-center pb-5'>Upcoming Events</h1>
+            <div className='container'>
                 {/* Logo Cookies */}
-                <div>
+                <div className='row justify-content-center pb-5'>
                     <img
                         //src assignd required expression | incremental naming of the imgs using i
                         // default property --> is where the img has been saved. To render img, default property must be invoked  
                         src={require(`../../assets/images/newsletter/newsletterOne.webp`)}
                         alt="logoCookies"
-                        className="img-thumbnail"
-                        style={{ width: "20%" }}
+                        className="img-thumbnail img-fluid col-6 d-inline-flex"
+                        // style={{ width: "50%"}}
                     />
-                    <h2>Logo Cookies</h2>
-                    <p>Want to give the attendees at your corporate event or conference a memento that they won’t soon forget!?
-                        A cookie with your logo on it is an effective way to get your guests talking at their table.
-                        Trying to increase your brand recognition?
-                        Try this unique method thats surprisingly delicious and will get your customers to return for more! </p>
+                    <div className='row col-6'>
+                        <h2>Logo Cookies</h2>
+                        <p>Want to give the attendees at your corporate event or conference a memento that they won’t soon forget!?
+                            A cookie with your logo on it is an effective way to get your guests talking at their table.
+                            Trying to increase your brand recognition?
+                            Try this unique method thats surprisingly delicious and will get your customers to return for more! </p>
+                    </div>
                 </div>
 
                 {/* Warner Center Park */}
-                <div>
+                <div className='row justify-content-center d-inline-flex pb-5'>
+
                     <img
                         //src assignd required expression | incremental naming of the imgs using i
                         // default property --> is where the img has been saved. To render img, default property must be invoked  
                         src={require(`../../assets/images/newsletter/newsletterTwo.webp`)}
                         alt="parkMovies"
-                        className="img-thumbnail"
-                        style={{ width: "20%" }}
+                        className="img-thumbnail img-fluid col-6 d-inline-flex"
+                        // style={{ max-width: "20%" }}
                     />
-                    <h2>Movies at Warner Center Park!</h2>
-                    <p>Address: 5800 Topanga Canyon Blvd. Woodland Hills 91367</p>
-                    <p>Info: Ever Saturday in August at 7:30pm catch a FREE movie in the park.</p>
+                    <div className='row col-6'>
+                        <h2>Movies at Warner Center Park!</h2>
+                        <p>Address: 5800 Topanga Canyon Blvd. Woodland Hills 91367</p>
+                        <p>Info: Ever Saturday in August at 7:30pm catch a FREE movie in the park.</p>
+                    </div>
                 </div>
 
-                 {/* Back to School Cookies */}
-                 <div>
+                {/* Back to School Cookies */}
+                <div className='row justify-content-center pb-5'>
                     <img
                         //src assignd required expression | incremental naming of the imgs using i
                         // default property --> is where the img has been saved. To render img, default property must be invoked  
                         src={require(`../../assets/images/newsletter/newsletterThree.webp`)}
                         alt="schoolCookies"
-                        className="img-thumbnail"
-                        style={{ width: "20%" }}
+                        className="img-thumbnail img-fluid col-6 d-inline-flex"
+                        // style={{ width: "20%" }}
                     />
-                    <h2>Back to School Cookies</h2>
-                    <p>Check out our Back to School cookies available to order now! Gifts for teachers and students!</p>
+                    <div className='row col-6'>
+                        <h2>Back to School Cookies</h2>
+                        <p>Check out our Back to School cookies available to order now! Gifts for teachers and students!</p>
+                    </div>
                 </div>
 
                 {/* Farmers Market */}
-                <div>
-                    <h2>Check Us Throughout the Week at the Farmer's Market!</h2>
+                
+                <h2 className='text-center'>Check Us Throughout the Week at the Farmer's Market!</h2>
+                
+
+                <div  className=''>
+                    
                     <img
                         //src assignd required expression | incremental naming of the imgs using i
                         // default property --> is where the img has been saved. To render img, default property must be invoked  
@@ -67,7 +78,7 @@ function Newsletter() {
                         // default property --> is where the img has been saved. To render img, default property must be invoked  
                         src={require(`../../assets/images/newsletter/newsletterFarm2.webp`)}
                         alt="farmersMarket"
-                        className="img-thumbnail"
+                        className="img-thumbnail flex-fill"
                         style={{ width: "20%" }}
                     />
                     <img
@@ -75,7 +86,7 @@ function Newsletter() {
                         // default property --> is where the img has been saved. To render img, default property must be invoked  
                         src={require(`../../assets/images/newsletter/newsletterFarm3.webp`)}
                         alt="farmersMarket"
-                        className="img-thumbnail"
+                        className="img-thumbnail flex-fill"
                         style={{ width: "20%" }}
                     />
                     <img
@@ -83,7 +94,7 @@ function Newsletter() {
                         // default property --> is where the img has been saved. To render img, default property must be invoked  
                         src={require(`../../assets/images/newsletter/newsletterFarm4.webp`)}
                         alt="farmersMarket"
-                        className="img-thumbnail"
+                        className="img-thumbnail flex-fill"
                         style={{ width: "20%" }}
                     />
                     <img
@@ -91,10 +102,10 @@ function Newsletter() {
                         // default property --> is where the img has been saved. To render img, default property must be invoked  
                         src={require(`../../assets/images/newsletter/newsletterFarm5.webp`)}
                         alt="farmersMarket"
-                        className="img-thumbnail"
+                        className="img-thumbnail  flex-fill"
                         style={{ width: "20%" }}
                     />
-                    
+                   
                 </div>
             </div>
         </section>
