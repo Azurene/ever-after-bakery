f21c50b5f8cc156f50c4c13ec776869bc7755435
Angel Santos <iiisantosjangel@gmail.com>
2022-08-08 19:54:14 -0700

    worked on navbar updated the path

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,7 +4,7 @@ function Header(props) {
     return (
 
         <header>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-black">
+            <nav className="navbar navbar-expand-lg navbar-white bg-white">
                 {/* <a className="navbar-brand" href="/"></a> */}
                 {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -13,32 +13,32 @@ function Header(props) {
                     <ul className="navbar-nav mr-auto">
                         {/* home */}
                         <li className="nav-item active">
-                            <a className="nav-link  "><button type='button' class='btn btn-black text-white'  onClick={() => {
+                            <a className="nav-link" href='/'><button type='button' class='btn btn-white text-black'  onClick={() => {
                             props.setCurrentPage("Header")}} >Home</button> <span  className="sr-only"> </span></a>
                         </li>
                         {/* about */}
                         <li className="nav-item active">
-                            <a className='nav-link'><button type='button' class='btn btn-black text-white' onClick={() => {
+                            <a className='nav-link' href='../components/Header/index.js'><button type='button' class='btn btn-white text-black' onClick={() => {
                                 props.setCurrentPage('About')}}>About</button><span className='sr-only'></span> </a>
                             
                                     </li>
                         {/* baked goods */}
                         <li className="nav-item active">
-                            <a className="nav-link"><button type='button' class='btn btn-black text-white' onClick={() => {
+                            <a className="nav-link" href='../components/Baked-Goods/index.js'><button type='button' class='btn btn-white text-black' onClick={() => {
                                 props.setCurrentPage('Baked-Goods')}}>Baked Goods</button><span  className='sr-only'></span></a>
                         </li>
                         {/* newsletter */}
                         <li className="nav-item active">
-                            <a className="nav-link"><button type='button' class='btn btn-black text-white' onClick={() => { 
+                            <a className="nav-link" href='../components/Newspaper/index.js'><button type='button' class='btn btn-white text-black' onClick={() => { 
                             props.setCurrentPage('Newsletter')}}>Newsletter</button><span  className='sr-only'></span></a>
                         </li>
                         {/* contact */}
                         <li className="nav-item active">
-                            <a className="nav-link"><button type='button' class='btn btn-black text-white' onClick={()  => {
+                            <a className="nav-link" href="../components/Contact/index.js"><button type='button' class='btn btn-white text-black' onClick={()  => {
                                 props.setCurrentPage('Contact')}}>Contact</button><span className='sr-only'></span></a>
                         </li>
                     </ul>
-                    <span className="navbar-text text-white ">
+                    <span className="navbar-text text-black ">
                         <a className="navbar" href="/">
                             <div className="logo-image">
                                 <img 
